e020d46d4f8781346d18b18d90e6ad6271b20a1c
munyah <munyaradzimagodo@gmail.com>
2026-02-25 16:28:49 +0200

    created a curated s3 bucket for storing processed data

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -2,3 +2,8 @@ module "raw_bucket" {
     source = "../../modules/s3"
     bucket_name = "customer-platform-dev-raw-munya-001"
 }
+
+module "curated_bucket" {
+    source = "../../modules/s3"
+    bucket_name = "customer-platform-dev-curated-munya-002"
+}
