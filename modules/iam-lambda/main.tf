521a48ba0a934592225ac6ae07b66fcc24e8a472
munyah <munyaradzimagodo@gmail.com>
2026-03-04 22:20:51 +0200

    attached CloudWatch Logs policy to lambda role

--- modules/iam-lambda/main.tf
+++ modules/iam-lambda/main.tf
@@ -1,15 +1,21 @@
 # Lambda execution role (assumed by AWS Lambda)
 resource "aws_iam_role" "lambda_exec" {
-    name = "lambda-exec-${var.environment}"
+  name = "lambda-exec-${var.environment}"
 
-    assume_role_policy = jsonencode({
-        version = "2012-10-17"
-        statement = [{
-            Effect = "Allow"
-            Principal = {
-                Service = "lambda.amazonaws.com"
-            }
-            Action = "sts:AssumeRole"
-        }]
-    })
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+# Allow Lambda to write logs to CloudWatch
+resource "aws_iam_role_policy_attachment" "lambda_logs" {
+  role = aws_iam_role.lambda_exec.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
