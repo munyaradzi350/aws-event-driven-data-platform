dc3e21873703cd0b2f25e51d153b60a5591c3210
munyah <munyaradzimagodo@gmail.com>
2026-03-17 12:27:29 +0200

    allowing lambda to start Glue jobs via glue:StartJobRun permission

--- modules/iam-lambda/main.tf
+++ modules/iam-lambda/main.tf
@@ -19,3 +19,27 @@ resource "aws_iam_role_policy_attachment" "lambda_logs" {
   role       = aws_iam_role.lambda_exec.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
+
+# Allow Lambda to start Glue jobs
+resource "aws_iam_policy" "lambda_glue_start" {
+  name = "lambda-glue-start-${var.environment}"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "glue:StartJobRun"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+# Attach the Glue policy to Lambda role
+resource "aws_iam_role_policy_attachment" "lambda_glue_attach" {
+  role = aws_iam_role.lambda_exec.name
+  policy_arn = aws_iam_policy.lambda_glue_start.arn
+}
--- modules/iam-lambda/outputs.tf
+++ modules/iam-lambda/outputs.tf
@@ -1,10 +1,10 @@
 # Export the Lambda role ARN for use in other modules
 
-output "role_arn" {
+output "lambda_role_arn" {
   value = aws_iam_role.lambda_exec.arn
 }
 
 # Export the role name (useful for attachments later)
-output "role_name" {
+output "lambda_role_name" {
   value = aws_iam_role.lambda_exec.name
 }
