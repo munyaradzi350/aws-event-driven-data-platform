c907088884eeca0fed1ef2fbe69db206cf19f842
munyah <munyaradzimagodo@gmail.com>
2026-03-19 11:23:14 +0200

    exposing Lambda function name and ARN outputs

--- modules/lambda/main.tf
+++ modules/lambda/main.tf
@@ -1,10 +1,13 @@
 # This tells Terraform to create an AWS Lambda function
 resource "aws_lambda_function" "this" {
-# This sets the actual Lambda function name in AWS
+  # This sets the actual Lambda function name in AWS
   function_name = var.function_name
-#  
+  #  Lambda must assume an IAM role when it runs.
   role = var.role_arn
+  # This tells AWS which runtime to use for the Lambda function.  
   runtime = var.runtime
+  # This tells Lambda where execution starts.  
   handler = var.handler
+  #This tells Terraform where the zipped Lambda deployment package is. Terraform will upload that zip file to AWS Lambda. 
   filename = var.filename
 }
