c0574e855afeb0bae57da86b73e2107ec6ba84c3
munyah <munyaradzimagodo@gmail.com>
2026-03-19 16:39:01 +0200

    exposing Lambda function outputs from dev environment

---
+++ environments/dev/outputs.tf
@@ -0,0 +1,9 @@
+output "lambda_function_name" {
+  description = "Name of the deployed Lambda function"
+  value = module.lambda_trigger.function_name
+}
+
+output "lambda_function_arn" {
+  description = "ARN of the deployed Lambda function"
+  value = module.lambda_trigger.function_arn
+}
