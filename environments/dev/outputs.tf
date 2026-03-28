e4245480cfc5f7d5abe09bcc446e95138c296cd5
munyah <munyaradzimagodo@gmail.com>
2026-03-28 12:13:55 +0200

    exposed Glue job name and ARN in the dev environment

--- environments/dev/outputs.tf
+++ environments/dev/outputs.tf
@@ -7,3 +7,13 @@ output "lambda_function_arn" {
   description = "ARN of the deployed Lambda function"
   value       = module.lambda_trigger.function_arn
 }
+
+output "glue_job_name" {
+  description = "Name of the Glue job in dev"
+  value       = module.glue_job.job_name
+}
+
+output "glue_job_arn" {
+  description = "ARN of the Glue job in dev"
+  value       = module.glue_job.job_arn
+}
