311e5f556039fc0de206934d020ed4493b5bf112
munyah <munyaradzimagodo@gmail.com>
2026-04-03 14:51:17 +0200

    reTesting GitHub CI/CD workflow

--- scripts/lambda/lambda_function.py
+++ scripts/lambda/lambda_function.py
@@ -5,5 +5,5 @@ def lambda_handler(event, context):
 
     return {
         "statusCode": 200,
-        "body": json.dumps("Testing Lambda deployment automated through GitHub Actions CI/CD")
+        "body": json.dumps("Testing Lambda deployment automated through GitHub Actions CI/CD!!!")
     }
