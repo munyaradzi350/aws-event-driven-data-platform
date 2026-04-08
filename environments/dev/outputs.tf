5187e96ac9cff2c9853cac590b71c2db02d2516b
munyah <munyaradzimagodo@gmail.com>
2026-04-08 20:50:12 +0200

    updated the dev outputs.tf for github actions ARN

--- environments/dev/outputs.tf
+++ environments/dev/outputs.tf
@@ -20,5 +20,5 @@ output "glue_job_arn" {
 
 output "github_actions_role_arn" {
   description = "ARN of the IAM role for GitHub Actions"
-  value = aws_iam_role.github_actions_role.arn
+  value       = module.iam_github_oidc.github_actions_role_arn
 }
