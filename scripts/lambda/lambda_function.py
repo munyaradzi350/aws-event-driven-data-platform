c87627ba71d38d2546cbf7b465d74a855d02dc42
munyah <munyaradzimagodo@gmail.com>
2026-06-05 10:15:34 +0200

    reTesting again the lambda deployment after allowing GitHub Actions to read the Lambda configurations

--- scripts/lambda/lambda_function.py
+++ scripts/lambda/lambda_function.py
@@ -5,5 +5,5 @@ def lambda_handler(event, context):
 
     return {
         "statusCode": 200,
-        "body": json.dumps("Testing Lambda deployment automated through GitHub Actions CI/CD and CI/CD is now reading from ssm parameters store!!!")
+        "body": json.dumps("Testing Lambda deployment automated through GitHub Actions CI/CD and CI/CD is now reading from ssm parameters store!!!!")
     }
